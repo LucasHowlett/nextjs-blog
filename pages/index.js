99fd83b3c72700789564a4ae8ffdb43026b0bf43
Lucas Howlett <lucashowlett@Lucass-iMac.local>
2020-09-20 17:58:28 -0600

    first commit

---
+++ pages/index.js
@@ -0,0 +1,53 @@
+import Head from "next/head";
+import Layout, { siteTitle } from "../components/layout";
+import utilStyles from "../styles/utils.module.css";
+import { getSortedPostsData } from "../lib/posts";
+import Link from "next/link";
+import Date from "../components/date";
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData
+    }
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>{siteTitle}</title>
+      </Head>
+      <section className={utilStyles.headingMd}>
+        <p>
+          I have over 7 years of experience working in graphic design and
+          creative direction. Some of the skills that I've acquired through my
+          experience are; delivering creative and unique solutions for brand
+          identity, packaging, print and digital media. Currently, I work as
+          Carma's Creative Director, designing responsive websites and back-end
+          dashboard tools for our clients. Please reach out via email if you
+          would like to learn more about how my services can help you or your
+          company. Thank you!
+        </p>
+      </section>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
